e14917a3acb56ccbc8fcc5e342e9f3402ee41020
aevtushenko <aevtushenko@oggettoweb.com>
2024-11-21 10:15:51 +0300

    Add type check and check for 0

--- recursion/script.js
+++ recursion/script.js
@@ -1,14 +1,28 @@
-'use strict';
 // Рекурсия
-let result = 0;
+'use strict';
 
 function outputReverseNumber(item) {
-    if (item % 10 !== 0) {
-        result = 10 * result + item % 10;
-        outputReverseNumber((item - item % 10) / 10);
+    let result = 0;
+
+    function formatNumber(item) {
+        if (!Number.isInteger(item)) {
+            return new Error('Необходимо ввести число');
+        }
+        if (!(/^[1-9]+$/.test(item)) && !result) {
+            return new Error('Число не должно содержать 0');
+        }
+
+        if (item % 10 !== 0) {
+            result = 10 * result + item % 10;
+            formatNumber((item - item % 10) / 10);
+        }
+
+        return result;
     }
 
-    return result;
+    return formatNumber(item);
 }
 
-console.log(outputReverseNumber(987654));
+console.log(outputReverseNumber(9087324));
+console.log(outputReverseNumber(987324));
+console.log(outputReverseNumber('123123'));
